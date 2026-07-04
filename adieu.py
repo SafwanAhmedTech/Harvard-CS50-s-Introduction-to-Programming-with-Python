e02cc0a94df112a28ac137bfe93825e660742f49
SafwanAhmedTech <liy1233x@outlook.com>
2026-07-04 16:48:56 +0100

    Add files via upload
    Problem to solve:
    In a file called adieu.py, implement a program that prompts the user for names, one per line, until the user inputs control-d. Assume that the user will input at least one name. Then bid adieu to those names, separating two names with one and, three names with two commas and one and, and 𝑛 names with 𝑛 −1 commas and one and, as in the below:
    
    Adieu, adieu, to Liesl
    Adieu, adieu, to Liesl and Friedrich
    Adieu, adieu, to Liesl, Friedrich, and Louisa
    Adieu, adieu, to Liesl, Friedrich, Louisa, and Kurt
    Adieu, adieu, to Liesl, Friedrich, Louisa, Kurt, and Brigitta
    Adieu, adieu, to Liesl, Friedrich, Louisa, Kurt, Brigitta, and Marta
    Adieu, adieu, to Liesl, Friedrich, Louisa, Kurt, Brigitta, Marta, and Gretl
    
    Hints
    Note that the inflect module comes with quite a few methods, per pypi.org/project/inflect. You can install it with:
    pip install inflect

---
+++ adieu.py
@@ -0,0 +1,17 @@
+import inflect
+p = inflect.engine()
+
+names = []
+
+while True:
+    try:
+        name = input('Enter name: ')
+        names.append(name)
+    except EOFError:
+        print()
+        break
+
+names = p.join(names)
+
+print(f'Adieu, adieu, to {names}')
+
